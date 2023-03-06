c95d944aff9e70f788bab44c065b8871a794ad96
Neelumishra <neelumishra0397@gmail.com>
2023-03-06 22:53:29 +0530

    Second Assignment

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import { Fourth } from './fourth';
 function App() {
   return (
     <Fragment>
-      <First/>
+      <h1>I am heading one...from first component</h1>
+      <h2>I am heading two...from second component</h2>
+      <h3>I am heading three...from third component</h3>
+      <button>Click me!!</button>
+      {/* <First/>
       <Second/>
       <Third/>
-      <Fourth/>
+      <Fourth/> */}
     </Fragment>
   );
 }
