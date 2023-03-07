37141b86cb634ea0f79641b69db1872ee7a27a4c
Neelumishra <neelumishra0397@gmail.com>
2023-03-07 20:57:34 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -10,14 +10,8 @@ import { Fourth } from './fourth';
 function App() {
   return (
     <Fragment>
-      <h1>I am heading one...from first component</h1>
-      <h2>I am heading two...from second component</h2>
-      <h3>I am heading three...from third component</h3>
-      <button>Click me!!</button>
-      {/* <First/>
-      <Second/>
-      <Third/>
-      <Fourth/> */}
+     
+      
     </Fragment>
   );
 }
